7a40d38b4bed0ef267a45985f32d56e86b557a4b
Steffen Gates <sgates@ctacorp.com>
2013-12-19 13:05:17 -0500

    Added SyndicationResponse and Client stubs

--- src/main/java/com/ctac/syndication/sdk/Client.java
+++ src/main/java/com/ctac/syndication/sdk/Client.java
@@ -12,4 +12,6 @@ public class Client {
     public String getClientVersion(){
         return apiVersion;
     }
+
+
 }
---
+++ src/main/java/com/ctac/syndication/sdk/SyndicationResponse.java
@@ -0,0 +1,11 @@
+package com.ctac.syndication.sdk;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Steffen Gates
+ * Date: 12/19/13
+ * Time: 1:03 PM
+ */
+public class SyndicationResponse {
+
+}
